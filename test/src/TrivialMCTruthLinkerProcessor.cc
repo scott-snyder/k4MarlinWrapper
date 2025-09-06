bad94e644e0e290e4da4cfca1fc468d166ae3b74
sss <sss@karma>
2025-09-05 22:40:57 -0400

    Refresh of using

--- test/src/TrivialMCTruthLinkerProcessor.cc
+++ test/src/TrivialMCTruthLinkerProcessor.cc
@@ -23,28 +23,30 @@
 #include "IMPL/LCCollectionVec.h"
 #include "IMPL/LCRelationImpl.h"
 
+using namespace lcio;
+
 TrivialMCTruthLinkerProcessor aTrivialMCTruthLinkerProcessor;
 
 TrivialMCTruthLinkerProcessor::TrivialMCTruthLinkerProcessor() : marlin::Processor("TrivialMCTruthLinkerProcessor") {
-  registerInputCollection(lcio::LCIO::MCPARTICLE, "InputMCs", "Name of the input MCParticle collection", m_mcCollName,
+  registerInputCollection(LCIO::MCPARTICLE, "InputMCs", "Name of the input MCParticle collection", m_mcCollName,
                           std::string("MCParticles"));
 
-  registerInputCollection(lcio::LCIO::RECONSTRUCTEDPARTICLE, "InputRecos",
+  registerInputCollection(LCIO::RECONSTRUCTEDPARTICLE, "InputRecos",
                           "Name of the input ReconstructedParticle collection", m_recoCollName,
                           std::string("PseudoRecoParticles"));
-  registerOutputCollection(lcio::LCIO::LCRELATION, "OutputMCRecoLinks", "Name of the output Reco - MC Truth link collection",
+  registerOutputCollection(LCIO::LCRELATION, "OutputMCRecoLinks", "Name of the output Reco - MC Truth link collection",
                            m_relCollName, std::string("TrivialMCRecoLinks"));
 }
 
-void TrivialMCTruthLinkerProcessor::processEvent(lcio::LCEvent* evt) {
+void TrivialMCTruthLinkerProcessor::processEvent(LCEvent* evt) {
   const auto mcColl = evt->getCollection(m_mcCollName);
   const auto recoColl = evt->getCollection(m_recoCollName);
 
   // NOTE: Not using LCRelationNavigator here because the internal map might
   // screw up the order and we want an easy check afterwards
-  auto relationColl = new IMPL::LCCollectionVec(lcio::LCIO::LCRELATION);
-  relationColl->parameters().setValue("FromType", lcio::LCIO::MCPARTICLE);
-  relationColl->parameters().setValue("ToType", lcio::LCIO::RECONSTRUCTEDPARTICLE);
+  auto relationColl = new IMPL::LCCollectionVec(LCIO::LCRELATION);
+  relationColl->parameters().setValue("FromType", LCIO::MCPARTICLE);
+  relationColl->parameters().setValue("ToType", LCIO::RECONSTRUCTEDPARTICLE);
 
   for (int i = 0; i < mcColl->getNumberOfElements(); ++i) {
     auto relation = new IMPL::LCRelationImpl();
