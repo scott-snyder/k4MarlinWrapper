f1c939fc384446202979a3283eaad95cc5ca3eb8
sss <sss@karma>
2025-09-10 22:30:21 -0400

    Refresh of depr2

--- k4MarlinWrapper/src/components/EDM4hep2Lcio.cpp
+++ k4MarlinWrapper/src/components/EDM4hep2Lcio.cpp
@@ -16,6 +16,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
 #include "k4MarlinWrapper/converters/EDM4hep2Lcio.h"
 #include "GlobalConvertedObjectsMap.h"
 #include "StoreUtils.h"
